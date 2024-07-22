b36e89358b413bb91d82f6bab0e69da394139705
Veysel Arslan <veysel.arslan1288@gmail.com>
2024-07-21 22:45:32 -0500

    css-loader and babel loader

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -46,7 +46,22 @@ module.exports = () => {
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader']
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                ['@babel/preset-env', { targets: 'defaults'}]
+              ]
+            }
+          }
+        }
       ],
     },
   };
